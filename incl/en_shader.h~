4f00d311daee45622bb40378b365cb69e197abdd
Rawocar <drumstar@hotmail.de>
2016-02-12 00:07:16 +0100

    commit 012

--- incl/en_shader.h~
+++ incl/en_shader.h~
@@ -15,28 +15,4 @@
 #include "en_app.h"
 #include "en_file.h"
 
-///////////////////////////////////////////
-///////////////////////////////////////////
-// Funktionsdeklaration
-///////////////////////////////////////////
-///////////////////////////////////////////
-
-/*
-	Beschreibung:			Diese Funktion lädt einen Shader und gibt die ID
-										zurück.
-	1. Parameter:			Dateipfad
-	2. Parameter:			Die Art des Shader
-	3. Parameter:			Programmvariable
-	return:						Shader ID
-*/
-
-int load_shader(const char*, GLenum, CApp*);
-
-/*
-	Beschreibung:			Diese Funktion entlädt das Shader Programm.
-	1. Parameter:     Programmvariable
-*/
-
-void unload_shader(CApp*);
-
 #endif
--- src/gm_init.cpp
+++ src/gm_init.cpp
@@ -15,29 +15,29 @@ int init_models_menu(CApp * pApp)
 
   // Würfel
 
-	ppModel[0]->create_cube(0.6f, 128, 128, 200, 255);
+	ppModel[0]->create_cube(0.6f, 128, 128, 200, 255, MDL_COLOR);
 	ppModel[0]->set_mdl_pos(0.0f, 0.0f, -2.0f);
 
-	ppModel[1]->create_cube(0.6f, 128, 128, 200, 255);
+	ppModel[1]->create_cube(0.6f, 128, 128, 200, 255, MDL_COLOR);
 	ppModel[1]->set_mdl_pos(2.0f, 0.0f, -1.0f);
 
-	ppModel[2]->create_cube(0.6f, 128, 128, 200, 255);
+	ppModel[2]->create_cube(0.6f, 128, 128, 200, 255, MDL_COLOR);
 	ppModel[2]->set_mdl_pos(-2.0f, 0.0f, -1.0f);
 
-	ppModel[3]->create_square(1.3f, 130, 0, 130);
-	//ppModel[3]->set_texture_square("textures/tiles.bmp");
-	ppModel[3]->set_mdl_pos(0.0f, 1.0f, -1.0f);
+	ppModel[3]->create_square(1.3f, 130, 0, 130, MDL_TEXTURE);
+	ppModel[3]->set_texture_square("textures/tiles.bmp");
+	ppModel[3]->set_mdl_pos(0.0f, 1.0f, -6.0f);
 
-	exit_code = ppModel[4]->load_mdl_from_obj("models/trooper.obj");
-  ppModel[4]->set_mdl_pos(0.0f, -1.0f, 0.0f);
+	//exit_code = ppModel[4]->load_mdl_from_obj("models/trooper.obj");
+  //ppModel[4]->set_mdl_pos(0.0f, -1.0f, 0.0f);
 
-	ppModel[5]->create_sphere(0.5f, 40, 128, 128, 200);
+	ppModel[5]->create_sphere(0.5f, 40, 128, 128, 200, MDL_COLOR);
 	ppModel[5]->set_mdl_pos(-1.3f, 1.7f, 0.0f);
 
-	ppModel[6]->create_pyramid(0.5f, 0.9f, 128, 128, 200);
+	ppModel[6]->create_pyramid(0.5f, 0.9f, 128, 128, 200, MDL_COLOR);
 	ppModel[6]->set_mdl_pos(1.3f, 1.7f, 0.0f);
 
-	ppModel[7]->create_cone(0.5f, 0.9f, 16, 128, 128, 200);
+	ppModel[7]->create_cone(0.5f, 0.9f, 16, 128, 128, 200, MDL_COLOR);
 	ppModel[7]->set_mdl_pos(-2.0f, 2.6f, -2.0f);
 
 	return exit_code;
